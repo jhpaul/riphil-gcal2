a291fea2f91ba884da1e65f4dffef004e4b8e215
Jacob Harrison Paul <jacobhpaul@gmail.com>
2015-11-05 23:16:15 -0500

    working with promises, can list events

--- pull_calendars.js
+++ pull_calendars.js
@@ -36,14 +36,12 @@ var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
 
 // Time Handling
 /* use a function for the exact format desired... */
-function ISODateString(d){
- function pad(n){return n<10 ? '0'+n : n}
- return d.getUTCFullYear()+'-'
-      + pad(d.getUTCMonth()+1)+'-'
-      + pad(d.getUTCDate())+'T'
-      + pad(d.getUTCHours())+':'
-      + pad(d.getUTCMinutes())+':'
-      + pad(d.getUTCSeconds())+'Z'}
+function ISODateString(d) {
+    function pad(n) {
+        return n < 10 ? '0' + n : n
+    }
+    return d.getUTCFullYear() + '-' + pad(d.getUTCMonth() + 1) + '-' + pad(d.getUTCDate()) + 'T' + pad(d.getUTCHours()) + ':' + pad(d.getUTCMinutes()) + ':' + pad(d.getUTCSeconds()) + 'Z'
+}
 
 // var d = new Date();
 // console.log(d, ISODateString(d)); // prints something like 2009-09-28T19:03:12Z
@@ -51,14 +49,14 @@ function ISODateString(d){
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Calendar API.
-  // authorize(JSON.parse(content), listEvents);
-  authorize(JSON.parse(content), execute);
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Calendar API.
+    // authorize(JSON.parse(content), listEvents);
+    authorize(JSON.parse(content), execute);
 });
 
 /**
@@ -69,21 +67,21 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
 }
 
 /**
@@ -95,27 +93,27 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
     });
-  });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+        rl.close();
+        oauth2Client.getToken(code, function(err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
 }
 
 /**
@@ -124,65 +122,68 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
     }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
 }
 
 // Begin Calendar Logic
 
-// execute with auth
-function execute(auth) {
-  getCalendars(auth)
-    .then(listCalendars)
-    .then(function(val){
-        console.log("yes")
-        val.forEach(function(cal){
-            console.log("a", cal['id'])
-        })
-    })
-    .catch(function (error) {
-        throw new Error("Error: " + error)
-    })
-  // }
-  // listEvents(auth)
-
-// Batch Requesting Works, but looks like I need a different API Key, that's a pain so let's wait for now
-  // var batch = new Batchelor({
-  //     'uri':'https://www.googleapis.com/batch',
-  //     'method':'POST',
-  //     'auth':
-  //     {
-  //         'bearer': auth['credentials']['access_token']
-  //     },
-  //     'headers': {
-  //         'Content-Type': 'multipart/mixed'
-  //     }
-  // });
-  // batch.add({
-  //     'method':'GET',
-  //     'path':'/calendar/v3/users/me/calendarList'
-  // })
-  // batch.run(function(err, response){
-  //     console.log('response:', response);
-  //   //   res.json(response);
-  // });
-}
 
 var calendar = google.calendar('v3');
 
-var printDate = "2015-11-02" //to be changed to route "localhost/day/2015-11-02"
+var printDate = "2015-11-05" //to be changed to route "localhost/day/2015-11-02"
 var timeMin = moment.tz(printDate + " 00:00", timeZone).format() //"2015-11-03T00:00:00Z"
 var timeMax = moment.tz(printDate + " 23:59:59", timeZone).format() //"2015-11-03T23:59:59Z"
-console.log (timeZone)
-console.log ("StartTime:", timeMin)
-console.log ("EndTime:", timeMax)
+console.log(timeZone)
+console.log("StartTime:", timeMin)
+console.log("EndTime:", timeMax)
+
+// execute with auth
+function execute(auth) {
+    getCalendars(auth)
+        .then(function(calendars) {
+            getEvents(auth, timeMin, timeMax, calendars)
+        })
+
+
+
+
+    .catch(function(error) {
+            throw new Error("Error: " + error)
+        })
+        // }
+        // listEvents(auth)
+
+    // Batch Requesting Works, but looks like I need a different API Key, that's a pain so let's wait for now
+    // var batch = new Batchelor({
+    //     'uri':'https://www.googleapis.com/batch',
+    //     'method':'POST',
+    //     'auth':
+    //     {
+    //         'bearer': auth['credentials']['access_token']
+    //     },
+    //     'headers': {
+    //         'Content-Type': 'multipart/mixed'
+    //     }
+    // });
+    // batch.add({
+    //     'method':'GET',
+    //     'path':'/calendar/v3/users/me/calendarList'
+    // })
+    // batch.run(function(err, response){
+    //     console.log('response:', response);
+    //   //   res.json(response);
+    // });
+}
+
+
 
 // var promise = new Promise(function(resolve, reject) {
 //   // do a thing, possibly async, then…
@@ -196,96 +197,115 @@ console.log ("EndTime:", timeMax)
 // });
 
 function getCalendars(auth) {
-  // Return a new promise.
-  return new Promise(function(resolve, reject) {
-      authlocal = {auth:auth}
-      calendar.calendarList.list(authlocal,
-          function(err, response) {
-            if (err) {
-              reject(Error('The API returned an error: ' + err));
-          } else {
-            calendars = response.items;
-            // console.log(calendars)
-            resolve(calendars, auth)
-            // return calendars
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        authlocal = {
+            auth: auth
         }
-        });
+        calendar.calendarList.list(authlocal,
+            function(err, response) {
+                if (err) {
+                    reject(Error('The API returned an error: ' + err));
+                } else {
+                    calendars = response.items;
+                    // console.log(calendars)
+                    calendars.forEach(function(calendar) {
+                        console.log(calendar['summary'])
+                    })
+                    resolve(calendars, auth)
+                        // return calendars
+                }
+            });
 
-  });
+    });
 }
 
-function listCalendars(calendars) {
-    if (calendars) {
-        calendars.forEach(function (calendar) {
-            console.log("a",calendar['summary'])
+function getEvents(auth, timeMin, timeMax, calendars) {
+    // Return a new promise.
+    console.log(timeMin, timeMax)
+    return new Promise(function(resolve, reject) {
+        // Pull list of all events in a calendar
+        events = []
+        emptyCals = []
+        calendars.forEach(function(cal) {
+            console.log("Calendar Selected:", cal['summary'])
+            calendar.events.list({
+                auth: auth,
+                calendarId: cal['id'],
+                timeMin: timeMin,
+                timeMax: timeMax,
+                singleEvents: 'True',
+                orderBy: 'startTime'
+            }, function(err, response) {
+                // console.log(response)
+                if (err) {
+                    console.error("**** [", cal['summary'], '] The API returned an error: ' + err);
+                }
+                // console.log(response)
+                else if (response && response.items) {
+                    if (response.items.length > 0) {
+                        response.items.forEach(function(item) {
+                            events.push(item);
+                            console.log("[ " + cal['summary'] + " ] ", item['summary'])
+                        })
+                    } else {
+                        console.error("**** [", cal['summary'], "] No Events")
+                        emptyCals.push(cal);
+                    }
+                }
+            })
+        })
 
-    })
-    return calendars
-    }
-    return console.error();
+    });
 }
-//
-// function listCalendars(auth, done) {
-//   // Pull list of all Calendars in the Account
-//   authlocal = {auth:auth}
-//   calendar.calendarList.list(authlocal,
-//       function(err, response) {
-//         if (err) {
-//           console.log('The API returned an error: ' + err);
-//          return
-//         }
-//         calendars = response.items;
-//         // console.log(calendars)
-//         done(null, auth, calendars)
-//         // return calendars
-//     });
-//   // done(null, cals);
+
+
+
+
+// function getEvents(err, auth, cals) {
+//     if (err) {
+//         return;
+//     }
+//     // console.log(cals);
+//     cals.forEach( function (item) {
+//         // console.log(item['id'])
+//         listEvents(auth, item['id'], item['summary'], timeMin, timeMax, function(){} )
+//         // console.log(auth);
+//     })
 // }
-
-function getEvents(err, auth, cals) {
-    if (err) {
-        return;
-    }
-    // console.log(cals);
-    cals.forEach( function (item) {
-        // console.log(item['id'])
-        listEvents(auth, item['id'], item['summary'], timeMin, timeMax, function(){} )
-        // console.log(auth);
-    })
-}
 
 
 
 
 
 function listEvents(auth, calendarId, calendarName, timeMin, timeMax, done) {
-  // Pull list of all events in a calendar
-  calendar.events.list({
-    auth: auth,
-    calendarId: calendarId,
-    timeMin: timeMin,
-    timeMax: timeMax,
-    singleEvents: 'True',
-    orderBy: 'startTime'
-  }, function(err, response) {
-    if (err) {
-      console.log("\n[",calendarName, '] The API returned an error: ' + err, "\n");
-      return;
-  }
-  events = response.items;
-  if (events) {
-      if (events.length == 0) {
-      console.log("No Events: ", calendarName, calendarId)
-      return;
-    }
-    // else {
-    //       events.forEach(function(item) {
-    //         //   console.log(item['summary'])
-    //       })
-    //   }
-    done(null, auth, calendarId, calendarName, timeMin, timeMax, events)
-  }
+    // Pull list of all events in a calendar
+    calendar.events.list({
+        auth: auth,
+        calendarId: calendarId,
+        timeMin: timeMin,
+        timeMax: timeMax,
+        singleEvents: 'True',
+        orderBy: 'startTime'
+    }, function(err, response) {
+        if (err) {
+            console.log("\n[", calendarName, '] The API returned an error: ' + err, "\n");
+            return;
+        }
+        events = response.items;
+        if (events) {
+            if (events.length == 0) {
+                console.log("No Events: ", calendarName, calendarId)
+                return;
+            }
+            // else {
+            //       events.forEach(function(item) {
+            //         //   console.log(item['summary'])
+            //       })
+            //   }
+            done(null, auth, calendarId, calendarName, timeMin, timeMax, events)
+        }
 
-    // console.log(events)
-  });
+        // console.log(events)
+    });
 }
